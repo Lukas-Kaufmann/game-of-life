c02f2d1ee6af3562eff5f6cc558835a1dded29be
Lukas Kaufmann <lukas.kaufm@outlook.com>
2023-03-29 22:32:16 +0200

    controls load/save

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Conway's game of life implemented in typescript and rendered on an html5 canvas.
 
 ## Start
 
+When running it for the first time, to ensure all the dependencies are installed, run:
 
+``` npm install ```
+
+Then to start it run:
+
+```npm run start```
 
 ## Usage
+
+The simulation can be controlled with the controls in the top right. The state can be manually be changed by pressing the mouse. Moving the mouse while pressed also changes the cells.
--- src/js/game.ts
+++ src/js/game.ts
@@ -21,6 +21,10 @@ const neighboursOffsets = [
 //state
 let cells: Cell[] = []
 
+let playing = true
+let drawing = true
+let interval = 200
+
 function start(renderer : Renderer) : void {
 	// Any setup that is required that only runs once before game loads goes here
 
@@ -30,33 +34,37 @@ function start(renderer : Renderer) : void {
 	}
 	
 	window.requestAnimationFrame(runRender)
-	setInterval(gameLoop, 200) //TODO wrap game loop in function that allows ui to stop and start the game
+	gameLoop()
 }
 
 function gameLoop(): void {
-	addDeadNeighbours()
+	if (playing) {
+		addDeadNeighbours()
 
-	cells.map((c) => {
-		let count = neighboursOffsets.map(({x, y}) => cells.find((nCell) => nCell.x == c.x + x && nCell.y == c.y + y)).filter(c => c != undefined && c.alive).length
+		cells.map((c) => {
+			let count = neighboursOffsets.map(({x, y}) => cells.find((nCell) => nCell.x == c.x + x && nCell.y == c.y + y)).filter(c => c != undefined && c.alive).length
 
-		let nextState = false
+			let nextState = false
 
-		switch (count) {
-			case 0:
-			case 1:
-				nextState = false
-				break;
-			case 2:
-				nextState = c.alive
-				break;
-			case 3:
-				nextState = true
-		}
-		return {
-			cell: c,
-			newState: nextState
-		}
-	}).forEach(({cell, newState}) => cell.alive = newState)
+			switch (count) {
+				case 0:
+				case 1:
+					nextState = false
+					break;
+				case 2:
+					nextState = c.alive
+					break;
+				case 3:
+					nextState = true
+			}
+			return {
+				cell: c,
+				newState: nextState
+			}
+		}).forEach(({cell, newState}) => cell.alive = newState)
+	}
+
+	setTimeout(gameLoop, interval)
 }
 
 function addDeadNeighbours() {
@@ -65,7 +73,6 @@ function addDeadNeighbours() {
 			let neighbour = cells.find((c) => c.x == cell.x + offset.x && c.y == cell.y + offset.y)
 			if (!neighbour) {
 				let newCell = new Cell(cell.x + offset.x, cell.y + offset.y, false)
-				//connect(newCell)
 				cells.push(newCell)
 			}
 		}
@@ -77,23 +84,72 @@ function handleCellEvent(x : number, y: number) {
 
 	let cell = cells.find( c => c.x == x && c.y == y)
 	if (!cell) {
-		let newCell = new Cell(x, y, true)
-		//connect(newCell)
+		let newCell = new Cell(x, y, drawing)
 		cells.push(newCell)
 	} else {
-		cell.alive = true
+		cell.alive = drawing
 	}
+}
+
+const MAX_INTERVAL = 5000
+const MIN_INTERVAL = 50
+
+function toInterval(perc : number) : number {
+	let delta = MAX_INTERVAL - MIN_INTERVAL
+
+	return (1/perc * delta) + MIN_INTERVAL
+	
 }
 
 window.onload = () => {
 
-	for (let i = 20; i < 70; i+=1) {
-		for (let j = 20; j < 70; j+=1) {
-			if (Math.random() < 0.5) {
-				cells.push(new Cell(i, j, true))
+	document.getElementById("play").addEventListener("change", (ev) => {
+		playing = (ev.target as HTMLInputElement).checked
+	})
+
+	document.getElementById("draw").addEventListener("change", (ev) => {
+		drawing = (ev.target as HTMLInputElement).checked
+	})
+
+	document.getElementById("speed").addEventListener("change", (ev) => {
+		let t = Number(((ev.target as HTMLInputElement).value))
+		interval = toInterval(t)
+		
+	})
+
+	document.getElementById("load").onclick = () => {
+		let file = (document.getElementById("loadFile") as HTMLInputElement).files[0];
+		if (file) {
+			var reader = new FileReader();
+			reader.readAsText(file, "UTF-8");
+			reader.onload = function (evt) {
+				let loadedCells = JSON.parse(evt.target.result as string) as Cell[]
+				cells = loadedCells
 			}
 		}
 	}
+
+	document.getElementById("save").onclick = () => {
+		var fileContent = JSON.stringify(cells.filter((c) => c.alive))
+		var bb = new Blob([fileContent ], { type: 'application/json' });
+		var a = document.createElement('a');
+		a.download = 'state.json';
+		a.href = window.URL.createObjectURL(bb);
+		a.click();
+		a.remove()
+	}
+
+	document.getElementById("random").onclick = () => {
+		cells = []
+		for (let i = 20; i < 70; i+=1) {
+			for (let j = 20; j < 70; j+=1) {
+				if (Math.random() < 0.5) {
+					cells.push(new Cell(i, j, true))
+				}
+			}
+		}
+	}
+	
 	
 	let renderer = new Renderer(cellSize, handleCellEvent);
 
